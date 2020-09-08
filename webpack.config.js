a2f7f51a8846cc0b4b2f187f9e8552a13863e291
FernelyVRSe <fernelyveras@gmail.com>
2020-09-08 14:47:02 -0400

    All project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HTMLwebpackPlugin = require('html-webpack-plugin')
+
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  'style-loader',
+                  // Translates CSS into CommonJS
+                  'css-loader',
+                  // Compiles Sass to CSS
+                  'sass-loader',
+                ],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+        ]
+    },
+    plugins:[
+        new HTMLwebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
